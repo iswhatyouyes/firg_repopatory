ce029d706564a3900091466e5e8ef82137523248
iswhatyouyes g <07kalebg@gmail.com>
2024-09-12 18:06:10 +0000

    moogie

--- hello.py
+++ hello.py
@@ -1,1 +1,12 @@
-print("if you give a mouse a cookie")
+print("if you give a mouse a mookie")
+
+groceries.append("oranges")
+groceries.pop()
+groceries.pop
+
+print(groceries)
+groceries.inset(2, "bananas")
+
+groceries.sort()
+
+print(groceries)
